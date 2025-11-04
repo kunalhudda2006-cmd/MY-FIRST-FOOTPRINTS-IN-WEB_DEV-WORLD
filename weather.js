49c7aaa43b0602bbd9cc26cd4adbc9eb79e79cfb
kunalhudda2006-cmd <kunalhudda2006@gmail.com>
2025-11-04 19:00:10 +0530

    added favicon

---
+++ weather.js
@@ -0,0 +1,14 @@
+let but=document.querySelector("button");
+but.addEventListener("click",async()=>{
+    const apiKey = "db76ac0d5a7881b6ddc2e593bf0926a6";
+    const city=document.querySelector("#city").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    document.querySelector("#temp").textContent = `${data.main.temp}°C`;
+    document.querySelector("#desc").textContent = data.weather[0].description;
+    document.querySelector("#humidity").textContent = `${data.main.humidity}%`;
+    document.querySelector("#img").src=iconUrl;
+});
